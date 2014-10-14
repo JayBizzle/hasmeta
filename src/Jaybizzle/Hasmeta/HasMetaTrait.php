1df3a79646a95a577ac5c7c613aee1e78531935d
Mark Beech <mbeech@mark-beech.co.uk>
2014-10-14 13:45:36 +0100

    Initial commit

---
+++ src/Jaybizzle/Hasmeta/HasMetaTrait.php
@@ -0,0 +1,172 @@
+<?php 
+namespace Jaybizzle\Hasmeta;
+
+
+trait HasMetaTrait
+{
+
+	protected $metaData = NULL;
+	
+	protected function newMeta($meta, $extra = array()) {
+		$metaModel = new $this->meta_model();
+		$metaModel->{$this->meta_key_name} = $meta[$this->meta_key_name];
+		$metaModel->{$this->meta_value_name} = $meta[$this->meta_value_name];
+		$metaModel->{$this->meta_foreign_key} = $this->{$this->meta_primary_key};
+
+		if(!empty($extra)) {
+			foreach($extra as $key => $value) {
+
+			}
+		}
+		
+		$metaModel->employerID = $this->employerID;
+
+		return $metaModel;
+	}
+
+
+	/**
+	 * Get the data associated with this opportunity
+	 *
+	 * @return hasOne
+	 */
+	abstract public function meta();
+
+	/**
+	 * Dynamically retrieve attributes on the model.
+	 *
+	 * @param  string  $key
+	 * @return mixed
+	 */
+	public function __get($key) {
+		$value = $this->getAttribute($key);
+
+		if (is_null($value)) {
+			try {
+				if(isset($this->getMeta()->$key)) {
+					$value = $this->getMeta()->$key->dataValue;
+				} else {
+					$value = NULL;
+				}
+			} catch (Exception $e) {
+				$value = NULL;
+			}
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Dynamically set attributes on the model.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $value
+	 * @return void
+	 */
+	public function __set($key, $value) {
+		if (array_key_exists($key, $this->getAttributes())) {
+			$this->setAttribute($key, $value);
+			return;
+		}
+
+		try {
+			if (isset($this->getMeta()->$key)) {
+				$this->getMeta()->$key->dataValue = $value;
+				return;
+			}
+		} catch (Exception $e) { }
+
+		$columns = \DB::select('DESCRIBE '.$this->table);
+
+		foreach ($columns as $column) {
+			if ($column->Field === $key) {
+				$this->setAttribute($key, $value);
+				return;
+			}
+		}
+
+		$this->getMeta()->$key = $this->newMeta(array($this->meta_key_name => $key, $this->meta_value_name => $value));
+	}
+
+
+	public function __isset($key) {
+
+		if ((isset($this->attributes[$key]) || isset($this->relations[$key])) || ($this->hasGetMutator($key) && ! is_null($this->getAttributeValue($key)))) {
+			return true;
+		}
+
+		return isset($this->getMeta()->$key->dataValue);
+	}
+
+	public function push() {
+		parent::push();
+
+		// clear out current meta data
+		$this->deleteMeta();
+
+		// Save the meta data
+		$this->saveMeta();
+	}
+
+	public function save(array $options = array()) {
+		parent::save();
+
+		// clear out current meta data
+		$this->deleteMeta();
+
+		// Save the meta data
+		$this->saveMeta();
+	}
+
+	/**
+	 * Get the data associated with this model in a usable format
+	 *
+	 * @return array
+	 */
+	protected function getMeta() {
+		if (is_null($this->metaData)) {
+			$primaryKey = $this->primaryKey;
+
+			if (is_null($this->$primaryKey)) {
+				// If nothing has been set and there is no ID then there will be no data
+				$this->metaData = (object)[];
+			} else {
+				$meta = $this->meta;
+
+				dd($meta);
+				$niceDataArray = array();
+
+				foreach ($meta as $data) {
+					$niceDataArray[$data->{$this->meta_key_name}] = $data;
+				}
+
+				$this->metaData = (object)$niceDataArray;
+			}
+		}
+		return $this->metaData;
+	}
+
+	protected function saveMeta() {
+		$primaryKey = $this->primaryKey;
+
+		foreach ((array)$this->getMeta() as $data) {
+			if (is_null($data->$primaryKey)) {
+				$data->$primaryKey = $this->$primaryKey;
+			}
+			
+			if(!is_null($data->{$this->meta_value_name}))
+				$data->save();
+		}
+	}
+
+	protected function deleteMeta() {
+		$primaryKey = $this->primaryKey;
+
+		foreach ((array)$this->getMeta() as $data) {
+			if(is_null($data->{$this->meta_value_name})) {
+				$dataID = $this->meta_primary_key;
+				$data->destroy($data->$dataID);
+			}
+		}
+	}
+}
